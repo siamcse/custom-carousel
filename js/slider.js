b78ba4165985cbc74eb0d318c096e3547f01395f
siamcse <siamcse01@gmail.com>
2022-09-15 23:19:49 +0600

    done

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+const images = [
+    "images/pic-1.jpg",
+    "images/pic-2.jpg",
+    "images/pic-3.jpg",
+    "images/pic-4.jpg",
+    "images/pic-5.jpg",
+    "images/pic-6.jpg",
+    "images/pic-7.jpg",
+    "images/pic-8.jpg",
+    "images/pic-9.jpg"
+];
+let imgIndex = 0;
+const img = document.getElementById('carousel-img');
+setInterval(() =>{
+    if(imgIndex === images.length){
+        imgIndex = 0;
+    }
+    img.setAttribute('src',images[imgIndex]);
+    imgIndex++;
+},1000)
